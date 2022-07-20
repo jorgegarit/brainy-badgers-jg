e629bbe6cd94613ff4c7939d428f19eac9d4a3c1
Jorge Garit <jorgegarit@gmail.com>
2022-07-20 16:51:51 -0400

    am nhow able to generate a recipe on screen with a url to the recipe instructions that will open in a new tab if clicked.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,17 @@
-// creating function to call spoonacular and display results onto page
 
-// This will call the source url where the recipe lives and display it on the page 
-function recipeSource(id){
+// Array so that the recipe button generates a random recipe from the array, still work in progress
+var recipeArray = [];
+var randomRecipe = recipeArray[Math.floor(Math.random() * recipeArray.length)];
+
+// this function will generate a recipe on screen from the api call and the function will include title of dish, an image of the dish and the link to the recipe
+function generateRecipe(query){
+    
     $.ajax({
-        url:"https://api.spoonacular.com/recipes/" + id + "/information?apiKey=d5f1707aa8a94f70a3fce40a554aebc6",
+        url:"https://api.spoonacular.com/recipes/search?apiKey=d5f1707aa8a94f70a3fce40a554aebc6&number=50&query="+query,
         success: function(res){
-            document.getElementsById("recipeLink").innerhHTML = res.sourceUrl;
-            document.getElementById("recipeLink").href=res.sourceUrl;
+            document.getElementById("recipeCall").innerHTML = "<h3>" + res.results[0].title + "</h3><br><img src='" + res.baseUri + res.results[0].image + "'width='300'/><br>" + "Go to recipe: ";
+            document.getElementById("recipeLink").innerHTML = res.results[0].sourceUrl;  
         }
     });
 }
 
-
-function generateRecipe(q){
-    $.ajax({
-        url:"https://api.spoonacular.com/recipes/search?apiKey=d5f1707aa8a94f70a3fce40a554aebc6&number=1&query="+q,
-        success: function(res){
-            document.getElementById("recipeCall").innerHTML = "<h3>" + res.results[0].title + "</h3><br><img src='" + res.baseUri + res.results[0].image + "'width='400' />";
-            getsource(res.results[0].id)
-        }
-    });
-}
